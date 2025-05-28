527144343c0dcdbacc591faf0b9fdff72bb3839f
das_kmuzykantov <kmuzdeveloper@gmail.com>
2025-05-28 16:28:13 +0300

    cart light

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -10,6 +10,7 @@ import Button from "primevue/button";
 import DeliveryAddress from "./parts/DeliveryAddress.vue";
 
 const qtyVal = ref(1);
+const qtyVal2 = ref(1);
 
 const volumes = ref(
     [
@@ -19,59 +20,47 @@ const volumes = ref(
     ]
 );
 const selectedVolume = ref({name: '50ml', code: '50', price: 45});
+const selectedVolume2 = ref({name: '50ml', code: '50', price: 45});
 
-const shipmentMethod = ref({
-  low: {
-    active: true,
-    basePrice: 15
-  },
-  normal: {
-    active: false,
-    basePrice: 20
-  },
-  fast: {
-    active: false,
-    basePrice: 23
-  }
-})
 const shipmentPrice = ref(15);
 
 const priceTotal = computed(() => {
-  return selectedVolume.value.price * qtyVal.value;
-})
+  return (selectedVolume.value.price * qtyVal.value);
+});
+const priceTotal2 = computed(() => {
+  return selectedVolume2.value.price * qtyVal2.value;
+});
 
-const priceWithDeliveryAndTax = computed(() => {
-  return priceTotal.value + (priceTotal.value * 0.2);
-})
 
-const addVal = () => {
-  qtyVal.value += 1;
+const addVal = (key=false) => {
+  if (key) {
+    qtyVal2.value += 1;
+  } else {
+    qtyVal.value += 1;
+  }
+
 }
 
-const decreaseVal = () => {
-  if (qtyVal.value >=2) {
-    qtyVal.value = qtyVal.value - 1;
+const decreaseVal = (key=false) => {
+
+  if (key) {
+    if (qtyVal2.value >=2) {
+      qtyVal2.value = qtyVal2.value - 1;
+    }
+  } else {
+    if (qtyVal.value >=2) {
+      qtyVal.value = qtyVal.value - 1;
+    }
   }
 }
 
-const animationShakeElement = () => {
-
-}
-
-const changeShipment = (key) => {
-  shipmentMethod.value.low.active = false;
-  shipmentMethod.value.normal.active = false;
-  shipmentMethod.value.fast.active = false;
-
-  shipmentMethod.value[key].active = true;
-  shipmentPrice.value = shipmentMethod.value[key].basePrice;
-}
 </script>
 
 <template>
 <div class="row" style="margin-bottom: 30px">
   <div class="col-12"><h3>Order items:</h3></div>
 </div>
+<!--  Item 1-->
   <div class="row">
     <div class="col-2">
       <div style="height: 100%; display: flex; align-items: center;">
@@ -100,31 +89,42 @@ const changeShipment = (key) => {
     <div class="col-3"><p style="font-size: 1.4rem; font-weight: bold">$ {{priceTotal}}</p></div>
   </div>
   <Divider/>
+<!--  Item 2-->
+  <div class="row">
+    <div class="col-2">
+      <div style="height: 100%; display: flex; align-items: center;">
+        <i class="pi pi-box" style="font-size: 1.4rem"></i>
+      </div>
+    </div>
+
+    <div class="col-7">
+      <span style="color: #aa3cc8; margin-bottom: 10px;"><b>Super item 2</b></span>
+      <div style="display: flex;flex-direction: column; gap: 20px">
+        <div style="display: flex; overflow: hidden; align-items: baseline; justify-content: space-between;">
+          <span>Volume</span>
+          <Select v-model="selectedVolume2" size="small" :options="volumes" optionLabel="name" placeholder="" class="w-full md:w-56" />
+        </div>
+        <div style="display: flex; overflow: hidden; align-items: baseline;  justify-content: space-between;">
+          <span>Qty:</span>
+          <div style="display: flex; justify-content: space-around; align-items: baseline; gap: 20px">
+            <i class="pi pi-minus-circle" @click="decreaseVal(2)" style="font-size: 1.3rem; cursor: pointer"></i>
+            <div>{{ qtyVal2 }}</div>
+            <i class="pi pi-plus-circle" @click="addVal(2)" style="font-size: 1.3rem; cursor: pointer"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-3"><p style="font-size: 1.4rem; font-weight: bold">$ {{priceTotal2}}</p></div>
+  </div>
+  <Divider/>
+
+
   <div style="width: 100%; text-align: right">
     <h3>Order summary:</h3>
     <p>Subtotal: ${{(selectedVolume.price * 0.2)+selectedVolume.price + shipmentPrice}}</p>
-    <p>Sales tax: ${{(selectedVolume.price * 0.2)}}</p>
+    <p>Sales tax: ${{selectedVolume.price * 0.2}}</p>
     <p>Shipment: ${{shipmentPrice}}</p>
-  </div>
-
-  <DeliveryAddress/>
-
-  <div style="display: flex; gap: 10px; margin: 40px 0 0 0; flex-direction: column;">
-    <div class="shipment-block" @click="changeShipment('low')">
-      <i :class="`pi pi-circle-fill ${shipmentMethod.low.active ? 'active-color' : ''}`"></i>
-      <div style="width: 60%">Cheap - 10 days</div>
-      <div>${{shipmentMethod.low.basePrice}}</div>
-    </div>
-    <div class="shipment-block" @click="changeShipment('normal')">
-      <i :class="`pi pi-circle-fill ${shipmentMethod.normal.active ? 'active-color' : ''}`"></i>
-      <div style="width: 60%">Fast - 5 days</div>
-      <div>${{shipmentMethod.normal.basePrice}}</div>
-    </div>
-    <div class="shipment-block" @click="changeShipment('fast')">
-      <i :class="`pi pi-circle-fill ${shipmentMethod.fast.active ? 'active-color' : ''}`"></i>
-      <div style="width: 60%">Express - 3 days</div>
-      <div>${{shipmentMethod.fast.basePrice}}</div>
-    </div>
   </div>
 
   <h3 style="margin-top: 50px">Add to order</h3>
@@ -135,39 +135,11 @@ const changeShipment = (key) => {
 
   <div class="buttons-container">
     <Button label="Continue shopping" variant="outlined"></Button>
-    <Button label="Payment"></Button>
+    <Button label="Checkout"></Button>
   </div>
 </template>
 
 <style scoped>
-.shipment-block {
-  display: flex;
-  padding: 10px;
-  align-items: center;
-  justify-content: space-between;
-  gap: 20px;
-  width: 100%;
-  position: relative;
-  border-radius: 6px;
-  box-shadow: 0 1px 21px 0 rgba(0,0,0,0.1);
-}
-
-.active-mark {
-  position: absolute;
-  top:0;
-  right: 0;
-  border-bottom-left-radius: 80%;
-  border-top-right-radius: 6px;
-  background-color: #71ce8f;
-  width: auto;
-  padding: 8px 10px 8px 12px;
-  color: white;
-  font-weight: bold;
-}
-
-.active-color {
-  color: #673ab7;
-}
 
 .buttons-container {
   display: flex;
